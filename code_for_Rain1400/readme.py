743584d1634529bc067b3e72cf0b053bace47c1b
HuangxingLin123 <58512753+HuangxingLin123@users.noreply.github.com>
2020-07-15 14:57:39 +0800

    Add files via upload

---
+++ code_for_Rain1400/readme.py
@@ -0,0 +1,18 @@
+
+## Train
+python train.py --dataroot ./datasets/train/rainy_image/ --name new2 --model derain
+
+
+# Test
+python test.py --dataroot ./datasets/test/rainy_image/ --name new --model derain
+
+
+# Test with our pretrained model
+python test.py --dataroot ./datasets/test/rainy_image/ --name pretrained --model derain
+
+
+
+######
+After the test, results are saved in './results/'.
+
+Run "psnr_and_ssim.py" to caculate psnr and ssim.
